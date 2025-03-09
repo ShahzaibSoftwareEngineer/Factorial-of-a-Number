3b40eae0708c7d18be76ba22a2ff7aa345423bd3
ShahzaibSoftwareEngineer <infoshahzaibmunir@gmail.com>
2025-03-09 09:28:52 -0700

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,16 +1,20 @@
+# Program 1
+
 # Factorial of a Number
 def factorial(n):
     return 1 if n == 0 else n * factorial(n-1)
 print(factorial(5)) 
 # 120
 
+# Program 2
 
 # Fibonacci Sequence
 n = 10
 a, b = 0, 1
 for _ in range(n):
     print(a, end=" ")
-    a, b = b, a + b
+    a, b = b, a + b3
+# Program 1
 
 # Palindrome Check
 def is_palindrome(s):
